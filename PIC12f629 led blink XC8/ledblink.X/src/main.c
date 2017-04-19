9aa8ee8218610ac3b384926e6553790336f01b06
rparthiban69 <rparthiban69@gmail.com>
2017-04-19 12:27:23 +0530

    Led blink pin configure
    GPIO pins Commit

--- PIC12f629 led blink XC8/ledblink.X/src/main.c
+++ PIC12f629 led blink XC8/ledblink.X/src/main.c
@@ -6,13 +6,13 @@ __CONFIG(FOSC_INTRCIO & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & BOREN_ON & CP_OFF & C
 void main()
 {
 
-    TRISIO      = 0; 
+    TRISIO      = 0;            
     GPIO        = 0; 
     __delay_ms(300);
-    GPIO4 = 1;                   
+    GPIO4 = 1;                  // GPIO 4 pin connected to Transistor      
     __delay_ms(4000);
     GPIO4 = 0;
-    __delay_ms(500);
+    __delay_ms(1000);
     while(1)
     {
         __delay_ms(1000);
